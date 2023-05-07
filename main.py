054c3a90d85039a2dde7f0e40df943c225088341
tyjung <tyjung1024@gmail.com>
2023-05-08 01:37:12 +0900

    fix incorrect visualization

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import open3d as o3d
+import laspy
 import numpy as np
 
-# 포인트 클라우드 데이터 로드
-input = '/Users/taeyeong/job/data/las/배경00_cloud.las'
-pcd = o3d.io.read_point_cloud("input.las", format="las")
+# las 파일 읽기
+las_file_path = '/Users/taeyeong/job/data/las/배경01_cloud.las'
+las_data = laspy.read(las_file_path)
 
-# 디스플레이 위치를 정의 (예: x, y, z 범위)
-display_position = {
-    "x_range": (0, 10),
-    "y_range": (0, 10),
-    "z_range": (0, 10),
-}
 
-# 디스플레이 위치에 해당하는 포인트를 필터링
-points = np.asarray(pcd.points)
-mask = (
-    (points[:, 0] >= display_position["x_range"][0])
-    & (points[:, 0] <= display_position["x_range"][1])
-    & (points[:, 1] >= display_position["y_range"][0])
-    & (points[:, 1] <= display_position["y_range"][1])
-    & (points[:, 2] >= display_position["z_range"][0])
-    & (points[:, 2] <= display_position["z_range"][1])
-)
-display_points = points[mask]
+# xyz 좌표 추출
+xyz_points = np.vstack((las_data.X, las_data.Y, las_data.Z)).T
 
-# 필터링된 포인트로 새로운 포인트 클라우드 생성
-display_pcd = o3d.geometry.PointCloud()
-display_pcd.points = o3d.utility.Vector3dVector(display_points)
+# 포인트 클라우드 생성
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(xyz_points)
 
-# 포인트 클라우드 시각화
-o3d.visualization.draw_geometries([display_pcd])
+# 디스플레이 설정
+display_x_range = (20000000, 22537186)
+display_y_range = (40000071, 46011203)
+display_z = -239972192
+
+# 디스플레이 뒷면의 점들 찾기
+behind_display_indices = np.where((xyz_points[:, 0] > display_x_range[0]) & (xyz_points[:, 0] < display_x_range[1]) &
+                                  (xyz_points[:, 1] > display_y_range[0]) & (xyz_points[:, 1] < display_y_range[1]) &
+                                  (xyz_points[:, 2] < display_z))[0]
+
+# 디스플레이 뒷면의 점들을 디스플레이에 표시
+xyz_points[behind_display_indices, 2] = display_z
+
+# 시각화
+o3d.visualization.draw_geometries([pcd])
